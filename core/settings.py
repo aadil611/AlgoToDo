e5b1aec4c628e68bc6da62b0048ba42b825f344d
aadil611 <ansariaadil611@gmail.com>
2023-06-01 17:30:37 +0530

    allowed hosts update

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG','False').lower() in ('true','1'))
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','todo.aadilansari.me']
 
 
 # Application definition
